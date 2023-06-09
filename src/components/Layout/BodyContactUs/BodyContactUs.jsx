ebcebc9f80fa5a801a633f36aa46ab8560c39876
caro109 <carogarcia.0419@gmail.com>
2023-06-09 17:41:46 -0500

    se hacen algunos cambios de contactanos

--- src/components/Layout/BodyContactUs/BodyContactUs.jsx
+++ src/components/Layout/BodyContactUs/BodyContactUs.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, Link } from "react";
 import { Footer } from "../Footer/Footer";
 import styles from "./BodyContactUs.module.css";
 
@@ -18,9 +18,15 @@ export const BodyContactUs = () => {
           className={`w-1/2 h-full bg-cover bg-black hidden md:block bg-center ${
             isLoading ? "bg-black" : "bg-black"
           } h-full`}></div> */}
-        <div className="hidden md:block w-1/2 h-screen bg-black"></div>
+        <div className="">
+          <img 
+            src="/FondoContactanos.webp" 
+            alt="" 
+            className="w-full h-full object-cover" 
+          />
+        </div>
         <div className="w-full md:w-3/5 h-full">
-          <div className="w-full h-96 ">
+          <div className="w-full h-80">
             <div className="md:p-10 flex flex-col gap-5 items-center ">
               <h1 className="text-bold text-5xl text-purple-600">
                 Contáctanos{" "}
@@ -33,10 +39,10 @@ export const BodyContactUs = () => {
               </p>
             </div>
           </div>
-          <div className="w-full h-2/5 flex flex-row justify-center">
-            <div className="w-full flex flex-col relative bottom-16 gap-14 items-center">
+          <div className="w-full h-2/4 flex flex-row justify-center">
+            <div className="w-2/3 flex flex-col relative bottom-16 gap-14 items-center bg-white bg-opacity-50 backdrop-filter backdrop-blur-lg border border-gray-200 rounded-lg shadow-2xl">
               <div className="">
-                <h2 className="text-3xl font-normal text-purple-600">
+                <h2 className="text-3xl font-bold text-purple-600 mt-3">
                   {" "}
                   Solicitud de información
                 </h2>
@@ -45,7 +51,7 @@ export const BodyContactUs = () => {
                 action=""
                 className="flex flex-col gap-3 w-full items-center">
                 <div className="flex flex-col w-full items-center">
-                  <label htmlFor="name">Nombre Completo</label>
+                  <label className="font-bold" htmlFor="name">Nombre Completo</label>
                   <input
                     className="border bg-gray-100 border-gray-300 rounded-md p-2 w-4/5 md:w-2/5 focus:outline-none focus:ring focus:ring-purple-800"
                     type="text"
@@ -54,7 +60,7 @@ export const BodyContactUs = () => {
                   />
                 </div>
                 <div className="flex flex-col w-full items-center">
-                  <label htmlFor="email">Correo Electronico:</label>
+                  <label className="font-bold bottom-auto" htmlFor="email">Correo Electronico:</label>
                   <input
                     className="border bg-gray-100 border-gray-300 rounded-md p-2 w-4/5 md:w-2/5 focus:outline-none focus:ring focus:ring-purple-800"
                     type="email"
@@ -63,7 +69,7 @@ export const BodyContactUs = () => {
                   />
                 </div>
                 <div className="flex flex-col w-full items-center">
-                  <label htmlFor="mensaje">Mensaje:</label>
+                  <label className="font-bold" htmlFor="mensaje">Mensaje:</label>
                   <textarea
                     className="h-40 px-4 py-2 bg-gray-100 border w-4/5 md:w-2/5 border-gray-300 rounded-md focus:outline-none focus:ring focus:ring-purple-800"
                     placeholder="Escribe tu mensaje aquí"
